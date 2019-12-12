b3d68da06eed6700427257e830345c37cd68d63b
Nick Sherman <ogleffeckt2@yahoo.com>
2019-12-12 16:54:01 -0500

    user can now edit id specifig blog posts

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -5,15 +5,23 @@ const blogReducer = (state, action) => {
 		case 'delete_blogpost':
 			return state.filter((blogPost) => blogPost.id !== action.payload);
 		case 'add_blogpost':
-			return [ ...state, { id: Math.floor(Math.random() * 99999), title: `Blog Post #${state.length + 1}` } ];
+			return [
+				...state,
+				{
+					id: Math.floor(Math.random() * 99999),
+					title: action.payload.title,
+					content: action.payload.content
+				}
+			];
 		default:
 			return state;
 	}
 };
 
 const addBlogPost = (dispatch) => {
-	return () => {
-		dispatch({ type: 'add_blogpost' });
+	return (title, content, callback) => {
+		dispatch({ type: 'add_blogpost', payload: { title, content } });
+		callback();
 	};
 };
 
@@ -23,4 +31,6 @@ const deleteBlogPost = (dispatch) => {
 	};
 };
 
-export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost, deleteBlogPost }, []);
+export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost, deleteBlogPost }, [
+	{ title: 'TEST POST', content: 'TEST CONTENT', id: 1 }
+]);
