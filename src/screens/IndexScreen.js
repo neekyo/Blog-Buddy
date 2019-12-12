895b2e85726f34be46c25e6606d6a44feda174d9
Nick Sherman <ogleffeckt2@yahoo.com>
2019-12-12 15:57:23 -0500

    added a create screen with a touchable create blog post button on index screen

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,9 +1,9 @@
 import React, { useContext } from 'react';
 import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'react-native';
-import { Context } from '../context/BlogContext';
+import { Context, Provider } from '../context/BlogContext';
 import { Feather } from '@expo/vector-icons';
 
-const IndexScreen = () => {
+const IndexScreen = ({ navigation }) => {
 	const { state, addBlogPost, deleteBlogPost } = useContext(Context);
 
 	return (
@@ -14,19 +14,31 @@ const IndexScreen = () => {
 				keyExtractor={(blogPost) => blogPost.title}
 				renderItem={({ item }) => {
 					return (
-						<View style={styles.row}>
-							<Text style={styles.title}>
-								{item.title} - {item.id}
-							</Text>
-							<TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
-								<Feather style={styles.icon} name="trash" />
-							</TouchableOpacity>
-						</View>
+						<TouchableOpacity onPress={() => navigation.navigate('Show', { id: item.id })}>
+							<View style={styles.row}>
+								<Text style={styles.title}>
+									{item.title} - {item.id}
+								</Text>
+								<TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+									<Feather style={styles.icon} name="trash" />
+								</TouchableOpacity>
+							</View>
+						</TouchableOpacity>
 					);
 				}}
 			/>
 		</View>
 	);
+};
+
+IndexScreen.navigationOptions = ({ navigation }) => {
+	return {
+		headerRight: (
+			<TouchableOpacity onPress={() => navigation.navigate('Create')}>
+				<Feather style={styles.create} name="plus" />
+			</TouchableOpacity>
+		)
+	};
 };
 
 const styles = StyleSheet.create({
@@ -43,6 +55,10 @@ const styles = StyleSheet.create({
 	},
 	icon: {
 		fontSize: 24
+	},
+	create: {
+		fontSize: 30,
+		marginRight: 6
 	}
 });
 
