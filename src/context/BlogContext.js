09cbb5372f33a9dd93cad76a7b4a5bf6059e2cdb
Nick Sherman <ogleffeckt2@yahoo.com>
2019-12-12 14:47:41 -0500

    refactored code to automate context creation

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,24 +1,18 @@
-import React, { useReducer } from 'react';
-
-const BlogContext = React.createContext();
+import createDataContext from './createDataContext';
 
 const blogReducer = (state, action) => {
 	switch (action.type) {
 		case 'add_blogpost':
-			return [ ...state, { title: `Blog Post #&{state.length +1}` } ];
+			return [ ...state, { title: `Blog Post #${state.length + 1}` } ];
 		default:
-			return;
+			return state;
 	}
 };
 
-export const BlogProvider = ({ children }) => {
-	const [ blogPosts, dispatch ] = useReducer([ blogReducer, [] ]);
-
-	const addBlogPost = () => {
+const addBlogPost = (dispatch) => {
+	return () => {
 		dispatch({ type: 'add_blogpost' });
 	};
-
-	return <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>{children}</BlogContext.Provider>;
 };
 
-export default BlogContext;
+export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost }, []);
